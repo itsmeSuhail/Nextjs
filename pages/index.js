85e78206e3589d7639a7d237f9b341537301cd19
itsmeSuhail <webdev12k@gmail.com>
2022-04-12 22:31:22 +0530

    only css

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,13 @@
 import React from 'react'
 import Game from './Game'
+/*
+1.we are using index.module.css
+  as external css
+  external css
+    <name>.module.css
 
+
+*/
 const index = () => {
   return<>
   <h1>We are going to use Global Css</h1>
